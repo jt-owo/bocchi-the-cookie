b650d675f1e9573fd22e4554ca34ac178ba9b8ef
jt-owo <jt-owo@outlook.com>
2024-06-18 01:51:51 +0200

    Replace wrinklers with bocchi.

--- main.js
+++ main.js
@@ -4,7 +4,19 @@ if (!BocchiTheCookie) var BocchiTheCookie = {
     isLoaded: false,
 
     init: function () {
+
+        // Cookie replacements.
+        // TODO: Replace gold cookie
         Game.Loader.Replace('perfectCookie.png', this.dir + '/bocchi.png');
+        
+        // Wrinkler replacements.
+        // TODO: maybe add more bocchi characters as wrinklers.
+        Game.Loader.Replace('wrinkler.png', this.dir + '/bocchi_wrinkler.png');
+        Game.Loader.Replace('winterWrinkler.png', this.dir + '/bocchi_winterWrinkler.png');
+        Game.Loader.Replace('shinyWrinkler.png', this.dir + '/bocchi_shinyWrinkler.png');
+        // TODO: Bocchi wrinkler shadows could look better.
+        Game.Loader.Replace('wrinklerShadow.png', this.dir + '/bocchi_wrinklerShadow.png');
+
         Game.Notify(`Bocchi The Cookie! is ready`, 'You can now give some headpats.', [0, 3]);
         this.isLoaded = true;
     },
