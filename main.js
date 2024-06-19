fbec8cd85d9c39c7d9b515f7b32e97cdc4231e5a
jt-owo <jt-owo@outlook.com>
2024-06-20 00:13:56 +0200

    Fix golden & wrath cookies not working & Add mod icon

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+function e(e, r) { const t = null === r[0]; let o = new RegExp(""); null !== r[0] && (o = "string" == typeof r[0] ? new RegExp(r[0].replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1"), "g") : r[0], o.test(e) || console.warn("Nothing to inject.")); const n = /(\)[^{]*{)/, c = /(}?)$/; switch (r[2]) { case "before": e = t ? e.replace(n, `$1${r[1]}`) : e.replace(o, `${r[1]}${r[0]}`); break; case "replace": e = t ? r[1] : e.replace(o, r[1]); break; case "after": e = t ? e.replace(c, `${r[1]}$1`) : e.replace(o, `${r[0]}${r[1]}`); break; default: throw new Error('where Parameter must be "before", "replace" or "after"') }return e } function injectCodes(r, t, o = {}) { let n = r.toString(); for (const r of t) n = e(n, r); const c = Function(...Object.keys(o), `return (${n})`)(...Object.values(o)); return c.prototype = r.prototype, c }
+
 if (!BocchiTheCookie) var BocchiTheCookie = {
     name: 'BocchiTheCookie',
     version: '1.1',
@@ -5,14 +7,28 @@ if (!BocchiTheCookie) var BocchiTheCookie = {
 
     init: function () {
         // Cookie replacements.
-        // TODO: Add reindeer 'cookies'
         Game.Loader.Replace('perfectCookie.png', this.dir + '/bocchi_cookie.png');
-        Game.Loader.Replace('goldCookie.png', this.dir + '/bocchi_goldCookie.png');
-        Game.Loader.Replace('wraithCookie.png', this.dir + '/bocchi_wrathCookie.png');
-        // Replaces contacts. TODO: Add special varians for bocchi contracts.
-        Game.Loader.Replace('contract.png', this.dir + '/bocchi_contract.png');
-        Game.Loader.Replace('wrathContract.png', this.dir + '/bocchi_wrathContract.png');
         
+        // TODO: Maybe add seasonal specific sprites.
+        switch (Game.baseSeason) {
+            default:
+                Game.Loader.Replace('goldCookie.png', this.dir + '/bocchi_goldCookie.png');
+                Game.shimmerTypes.golden.initFunc = injectCodes(
+                    Game.shimmerTypes.golden.initFunc,
+                    [
+                        ['img/goldCookie.png', `"${this.dir}/bocchi_goldCookie.png"`, 'replace'],
+                    ]
+                );
+        }
+
+        Game.Loader.Replace('wrathCookie.png', this.dir + '/bocchi_wrathCookie.png');
+        Game.shimmerTypes.golden.initFunc = injectCodes(
+            Game.shimmerTypes.golden.initFunc,
+            [
+                ['img/wrathCookie.png', `"${this.dir}/bocchi_wrathCookie.png"`, 'replace'],
+            ]
+        );
+
         // Wrinkler replacements.
         // TODO: maybe add more bocchi characters as wrinklers.
         Game.Loader.Replace('wrinkler.png', this.dir + '/bocchi_wrinkler.png');
@@ -25,7 +41,7 @@ if (!BocchiTheCookie) var BocchiTheCookie = {
         // TODO: Bocchi wrinkler shadows could look better.
         Game.Loader.Replace('wrinklerShadow.png', this.dir + '/bocchi_wrinklerShadow.png');
 
-        Game.Notify(`Bocchi The Cookie! is ready`, 'You can now give some headpats.', [0, 3]);
+        Game.Notify(`Bocchi The Cookie! is ready`, 'You can now give some headpats.', [0, 0, this.dir + '/icon.png'], 7);
         this.isLoaded = true;
 
         // TODO: Add options menu without relying on CCSE.
